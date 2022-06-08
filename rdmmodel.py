cf5aa273446864d478a844d7ecf0085cde37c566
LTakumi <louis.lemoine@polytechnique.edu>
2022-06-08 22:45:03 +0900

    fix setup file

---
+++ rdmmodel.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.base import BaseEstimator
+import pandas as pd
+
+class MyRandomRegressor(BaseEstimator):
+    """This model predicts random values between the mininimum and the maximum of y"""
+
+    def fit(self, X, y):
+        self.y_range = [np.min(y), np.max(y)]
+
+    def predict(self, X):
+        return np.random.uniform(self.y_range[0], self.y_range[1], size=X.shape[0])
+
