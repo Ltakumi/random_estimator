c62cccceb7afabf5ecefd3a12b594a07a5a56750
LTakumi <louis.lemoine@polytechnique.edu>
2022-06-08 22:14:10 +0900

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='random_estimator',
+   version='1.0',
+   description='A useful module',
+   author='Man Foo',
+   author_email='foomail@foo.com',
+   packages=['random_estimator'],  #same as name
+   install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+)
